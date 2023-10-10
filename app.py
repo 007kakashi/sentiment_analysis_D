4395cbcdf01dddda791efb3e5260729eb85b41a0
007kakashi <upendrasgosavi@gmail.com>
2023-10-10 17:38:10 +0530

    completed

--- app.py
+++ app.py
@@ -1,94 +1,74 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+# Import necessary libraries
 import pandas as pd
 import numpy as np
 import pickle 
 import streamlit as st
-from tensorflow import keras
-from keras.preprocessing.text import Tokenizer
-import nltk
 from nltk.stem import WordNetLemmatizer
+import nltk
 from nltk.corpus import stopwords
-# from keras.utils.data_utils import pad_sequences
-import regex as re
+import re
 from sklearn.feature_extraction.text import CountVectorizer
-from numpy import loadtxt
-from keras.models import load_model
-
-# load model
-# classifier = load_model('model.h5')
-
-open 
 
 filenametoken = 'model.pkl'
 classifier = pickle.load(open(filenametoken, 'rb'))
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
 
-# def clean(text):
-#     text = re.sub(r'[^\w\s]', '', text)
-#     text = re.sub(r'http\S+', '', text)
-#     text = text.lower()
-#     text = re.sub(r'\d+', '', text)
-#     text = ''.join([i for i in text if i.isalpha() or i.isspace()])
-#     tokens = nltk.word_tokenize(text)
-#     lemma= WordNetLemmatizer()
-#     tokens = [lemma.lemmatize(i) for i in tokens if not i in stopwords.words('english')]
-#     text = ' '.join(tokens)
-#     print(text)
+def clean(text):
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'http\S+', '', text)
+    text = text.lower()
+    text = re.sub(r'\d+', '', text)
+    text = ''.join([i for i in text if i.isalpha() or i.isspace()])
+    tokens = nltk.word_tokenize(text)
+    lemma= WordNetLemmatizer()
+    tokens = [lemma.lemmatize(i) for i in tokens if not i in stopwords.words('english')]
+    text = ' '.join(tokens)
+    print(text)
 
-#     return text
+    return text
 
-with open('cleaner.pkl','rb') as file:
-    cleaner=pickle.load(file)
+
+
+
+max_features = 13463
+vc = CountVectorizer(max_features=max_features, ngram_range=(1, 1))
+
+# sample_data = ["your text data here", "more text data", ...]
+# vc.fit(sample_data)
+
 
 def predict(text):
-    Class = classifier.predict(text,batch_size=1,verbose = 2)
-    print(Class)
-    y_predicted = Class.flatten()
-    print(y_predicted)
-    if y_predicted==0:
+    cleaned_text = clean(text)
+    
+    vectorized_text = vc.fit_transform([cleaned_text]).toarray()
+
+    if vectorized_text.shape[1] < max_features:
+        padding_size= max_features - vectorized_text.shape[1]
+        padded_data= np.pad(vectorized_text,((0,0),(0,padding_size)),'constant')
+    
+    predicted_class = classifier.predict(padded_data)
+    
+    if predicted_class == 0:
         return "Regular"
-    elif y_predicted==1:
+    elif predicted_class == 1:
         return "Sarcasm"
-    elif y_predicted==2:
+    elif predicted_class == 2:
         return "Irony"
-    elif y_predicted==3:     
+    elif predicted_class == 3:
         return "Figurative"
-    
 
-def execute():
+# Streamlit app
+def main():
+    st.title("Twitter Sentiment Analysis")
 
-  add_selectbox = st.selectbox("How would you like to predict?",("Sentence", "Batch File"))
+    add_selectbox = st.selectbox("How would you like to predict?", ("Sentence", "Batch File"))
 
-  st.title("Twitter Sentiment Analysis")
+    if add_selectbox == 'Sentence':
+        Sentence = st.text_input("Enter Text", key="account_length")
+        
+        if st.button("Predict"):
+            result = predict(Sentence)
+            st.success(f'Sentiment: {result}')
 
-  if add_selectbox == 'Sentence':
-      Sentence = st.text_input("Enter Text",key ="account_lenght")  
-     
-    
-      result=""
-      if st.button("Predict"):
-          cleaning=cleaner(Sentence)
-
-          vc =  CountVectorizer(ngram_range=(1,1))
-          x = vc.fit_transform(cleaning).toarray()
-
-          result=predict(x)
-          
-
-
-      st.success('{}'.format(result))
-      
-
-
-
-
-    
-
+if __name__=='__main__':
+    main()
